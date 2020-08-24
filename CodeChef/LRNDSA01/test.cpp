42b66318c6911378a8a11b3505939b7683565c60
vybhav72954 <49750343+vybhav72954@users.noreply.github.com>
2020-08-24 15:54:27 +0530

    Create test.cpp

---
+++ CodeChef/LRNDSA01/test.cpp
@@ -0,0 +1,14 @@
+//Problem Code:TEST
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i;
+    cin>>i;
+    while(i != 42){
+        cout<<i<<endl;
+        cin>>i;
+    }
+	// your code goes here
+	return 0;
+}
